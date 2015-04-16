19d3e3b384d27443c10cc14b14b80cd9af8bf5b7
MaritzaSintiTafur <maritzasintitafur@gmail.com>
2015-04-16 17:20:36 +0100

    Tercer avance

---
+++ javascripts/holi.js
@@ -0,0 +1,9 @@
+var tamaño= $(".contenedor")
+
+		{
+if (@media screen and (min-width: 600px)){
+	$(.fondo).hide();
+	$(.col-sm-6).toggleClass(".relative")
+}
+//espeor que eso se pued aleer
+//y que esto también
